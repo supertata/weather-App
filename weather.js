6867d7580b3823243ac071655ed2ec59ae39f8a2
Laetitia <laetitiachira@gmail.com>
2020-12-07 17:25:10 +0000

    Cleaned code

--- weather.js
+++ weather.js
@@ -8,10 +8,13 @@ $.ajax({
   success: function (result) {
     console.log(result);
     console.log(result.name);
-    $("#location").text(result.name);
+    let displayCity = `Weather for ${result.name}`;
+    $("#location").html(displayCity);
     let celsius = Math.round((result.main.temp - 273.15) * 10) / 10;
-    $("#temperature").text(celsius);
-    $("#wind").text(result.wind.speed);
-    $("#sky").text(result.weather[0].description);
+    $("#temperature").html(`Temperature: ${celsius}&#176;C`);
+    let displayWind = `Wind: ${result.wind.speed} m/s`;
+    $("#wind").html(displayWind);
+    let skyCondition = `Sky conditon: ${result.weather[0].description}`;
+    $("#sky").html(skyCondition);
   },
 });
