062ac50fe0d5d523fecf046ea226198601288fc9
Laetitia <laetitiachira@gmail.com>
2020-12-07 17:59:59 +0000

    added security to http request

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const url =
-  "http://api.openweathermap.org/data/2.5/weather?q=Manchester&APPID=8c49c01e775bfd7142bc8e6631eb89a0";
+  "https://api.openweathermap.org/data/2.5/weather?q=Manchester&APPID=8c49c01e775bfd7142bc8e6631eb89a0";
 
 $.ajax({
   url: url,
