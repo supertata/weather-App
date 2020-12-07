db194d2b5e0a0adb74bdd5c928efdbd2c64af23e
Laetitia <laetitiachira@gmail.com>
2020-12-07 17:02:16 +0000

    added weather data

--- weather.js
+++ weather.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const url =
+  "http://api.openweathermap.org/data/2.5/weather?q=Manchester&APPID=8c49c01e775bfd7142bc8e6631eb89a0";
+
+$.ajax({
+  url: url,
+  success: function (result) {
+    console.log(result);
+    console.log(result.name);
+    $("#location").text(result.name);
+    let celsius = Math.round((result.main.temp - 273.15) * 10) / 10;
+    $("#temperature").text(celsius);
+    $("#wind").text(result.wind.speed);
+    $("#sky").text(result.weather[0].description);
+  },
+});
